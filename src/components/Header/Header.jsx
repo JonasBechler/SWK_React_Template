ae22fe2f5524a2e3020244b24d8b767bdbfa42be
Jonas Bechler <jonasbechler@gmail.com>
2022-11-16 19:08:15 +0100

    same size header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,10 @@ export default function Header({ name, icon, setPage }) {
 	return (
 		<div className='KonstanzRed Row H_Center' onClick={onClickHandler}>
 
-			<img src={icon} alt="Bicycle" ></img>
+			<div className='V_Center H_Center' style={{ "fontSize": "60px" }}>
+				<img src={icon} alt="Icon" ></img>
+			</div>
+			
 			<div className='Text V_Center' style={{ "fontSize": "60px" }}>
 				{name}
 			</div>
