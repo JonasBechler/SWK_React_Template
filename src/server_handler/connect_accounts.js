313fc1a5554980a0884c68a57b6e82c55cd3aab0
JonasBechler <jonasbechler@gmail.com>
2023-02-01 21:00:16 +0100

    warning handling

--- src/server_handler/connect_accounts.js
+++ src/server_handler/connect_accounts.js
@@ -1,10 +1,7 @@
 module.exports = function(config){
     function connect_accounts() {
 
-        var url = new URL(`${config.device_ip}:${config.port}/api/connect`)
-
         window.location.href = `${config.device_ip}:${config.port}/kn/connect`
-        
         
     }
     return connect_accounts;
